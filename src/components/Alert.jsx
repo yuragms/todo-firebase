8b89ac93a8f7eec61b450ea6dc7181797087afeb
yuragms <yura.gms@gmail.com>
2022-11-20 14:48:50 +0200

    CSStransition

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,18 +1,34 @@
 import React, { useContext } from "react";
 import { AlertContext } from "../context/alert/alertContext";
+import { CSSTransition } from "react-transition-group";
 
 export const Alert = () => {
   const { alert, hide } = useContext(AlertContext);
-  if (!alert.visible) {
-    return null;
-  }
+  // if (!alert.visible) {
+  //   return null;
+  // }
   return (
-    <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
-      <strong>Внимание!&nbsp;{alert.text}</strong>
+    <CSSTransition
+      in={alert.visible}
+      timeout={{ enter: 500, exit: 350 }}
+      classNames="alert"
+      mountOnEnter
+      unmountOnExit
+    >
+      <div
+        className={`alert alert-${alert.type || "warning"} alert-dismissible`}
+      >
+        <strong>Внимание!&nbsp;{alert.text}</strong>
 
-      <button onClick={hide} type="button" className="close" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+        <button
+          onClick={hide}
+          type="button"
+          className="close"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
